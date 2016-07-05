4a405f8b4822a155fefa959262923802ef27d97d
prog <prog@DESKTOP>
2016-07-05 06:38:21 +0300

    умею искать путь к базе

--- Odin77Launcher/Program.cs
+++ Odin77Launcher/Program.cs
@@ -3,27 +3,100 @@ using System.Collections.Generic;
 using System.Text;
 // using System.Threading.Tasks;
 using Microsoft.Win32;
+using System.Windows.Forms;
+//using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace Odin77Launcher
 {
     class Program
     {
-        static string ProcessInput(string s)
+        static public string message = "";
+        static public string basePath = "";
+
+        static public bool findBase(string baseName)
         {
-            // TODO Verify and validate the input 
-            // string as appropriate for your application.
-            Console.WriteLine(s);
-            return s;
+            RegistryKey titlesKey = Registry.CurrentUser.OpenSubKey(@"Software\1C\1Cv7\7.7\Titles");
+            string[] paths = titlesKey.GetValueNames();
+            foreach(string path in paths)
+            {
+                if(titlesKey.GetValue(path).ToString().ToLower() == baseName)
+                {
+                    basePath = path;
+                    titlesKey.Close();
+                    // Console.WriteLine("Base path {0}", basePath);
+                    return true;
+                }
+            }
+            titlesKey.Close();
+            return false;
+        }
+
+        static public bool findProcess()
+        {
+
+            return false;
+        }
+        static bool ProcessInput(string url)
+        {
+            // Console.WriteLine("Started with {0}", url);
+            // odin77://baseName/objReference
+
+            string[] parts = url.Split('/');
+            if(parts.Length != 4)
+            {
+                message = "Неверная ссылка";
+                return false;
+            }
+            // на всякий случай
+            if(!parts[0].ToLower().Equals("odin77:"))
+            {
+                message = "Ошибка имени протокола";
+                return false;
+            }
+            // эт вот чтоб '//' обязательно было =%/
+            if(parts[1] != "")
+            {
+                message = "Ошибка адресации";
+                return false;
+            }
+            // поиск нужной базы в реестре
+            if(!findBase(parts[2].ToLower()))
+            {
+                message = "База с именем "+parts[2]+" не найдена";
+                return false;
+            }
+            // поиск запущенного процесса с нужным путём к базе
+            if(!findProcess())
+            {
+                message = "нет запущенного процесса 1С с нужным путём";
+                return false;
+            }
+            else
+            {
+                message = "процесс найден, запускаться пока не умею";
+                return false;
+            }
+
+
+            // если его нет - запущаем 1С
+
+            // вызов процесса с нужным объектом
+            Console.WriteLine("Starting base with {0}", parts[3]);
+            return true;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+            // Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
             foreach (string s in args)
             {
-                ProcessInput(s);
+                if (!ProcessInput(s))
+                {
+                    MessageBox.Show(message, "Ошибочка вышла", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
-            Console.WriteLine("\nPress any key to continue...");
+            // Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
         }
     }
