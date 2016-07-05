50de8a342d389424d84fbe0866ba1ced4a697e13
prog <prog@DESKTOP>
2016-07-05 05:44:48 +0300

    starting poing from sample

--- Odin77Launcher/Program.cs
+++ Odin77Launcher/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+// using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace Odin77Launcher
 {
     class Program
     {
+        static string ProcessInput(string s)
+        {
+            // TODO Verify and validate the input 
+            // string as appropriate for your application.
+            Console.WriteLine(s);
+            return s;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Raw command-line: \n\t" + Environment.CommandLine);
+            foreach (string s in args)
+            {
+                ProcessInput(s);
+            }
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
         }
     }
 }
